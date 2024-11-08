0e6cfe173be9e6796d73cde53bc368011a8920b2
Luis Fernando <fernando201022@hotmail.com>
2024-11-08 14:35:01 -0500

    Removing amazon logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,15 +17,8 @@ function Header() {
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-          <Image
-            onClick={() => router.push("/")}
-            src="https://links.papareact.com/f90"
-            width={150}
-            height={40}
-            objectFit="contain"
-            className="cursor-pointer"
-          />
+        <div style={{paddingInline: 15}} className="flex items-center flex-grow sm:flex-grow-0">
+          <p className="text-white">Campus Market</p>
         </div>
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
           <input
